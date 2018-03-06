6981504ff71fcc2b3c9b62a23bc97b4c88592e46
DESKTOP-NJV7KVA\Bence <bencetaylor@gmail.com>
2018-03-06 01:02:46 +0100

    lab2 2.7ig

--- adatblab2018/2.labor/neptun-2-csoportkod.sql
+++ adatblab2018/2.labor/neptun-2-csoportkod.sql
@@ -56,6 +56,42 @@ WHERE TIPUS='IC';
 prompt ]]>
 prompt </task>
 
+prompt <task n="2.4">
+prompt <![CDATA[
+SELECT vonatszam, tipus, megjegyzes
+FROM jarat
+WHERE (nap LIKE '1%') and (megjegyzes IS NOT NULL) and (NVL(kezd, TO_DATE('2012-01-01', 'YYYY-MM-DD')) <= TO_DATE('2012-01-01', 'YYYY-MM-DD')) and (NVL(vege, TO_DATE('2012-06-15', 'YYYY-MM-DD')) >= TO_DATE('2012-06-15', 'YYYY-MM-DD'))
+ORDER BY vonatszam;
+prompt ]]>
+prompt </task>
+
+prompt <task n="2.5">
+prompt <![CDATA[
+SELECT vonatszam, tipus, DECODE(megjegyzes, NULL, 'nincs megjegyzés', megjegyzes) AS MEGJEGYZES
+FROM jarat
+ORDER BY vonatszam;
+prompt ]]>
+prompt </task>
+
+prompt <task n="2.6">
+prompt <![CDATA[
+SELECT nev, varos FROM (SELECT nev, atlagutas/DECODE(SZTRAJKUTAS, 0, 1, SZTRAJKUTAS) as atlag, varos
+FROM allomas
+WHERE atlagutas/DECODE(SZTRAJKUTAS, 0, 1, SZTRAJKUTAS) > 0
+ORDER BY atlag)
+WHERE ROWNUM=1;
+prompt ]]>
+prompt </task>
+
+prompt <task n="2.7">
+prompt <![CDATA[
+SELECT allomas.varos as varos, megall.vonatszam as vonatszam
+FROM megall, allomas
+WHERE allomas.ID = megall.ALLOMAS_ID(+)
+ORDER BY varos, vonatszam;
+prompt ]]>
+prompt </task>
+
 prompt <task n="3.1">
 prompt <![CDATA[
 
